d9c0e26739750ffc102cdc6ac228cf89e023b05f
kenzaki1208 <duytrinh25072001@gmail.com>
2025-08-18 09:47:59 +0700

    update project

--- week11/Using_common_functions_in_SQL.sql
+++ week11/Using_common_functions_in_SQL.sql
@@ -19,3 +19,22 @@ select S.StudentId, S.StudentName, AVG(Mark)
 from Student S join Mark M on S.StudentId = M.StudentId
 group by S.StudentId, S.StudentName
 having AVG(Mark) >= ALL (select AVG(Mark) from Mark group by Mark.StudentId); #Sử dụng Having và All để tìm học viên có điểm trung bình lớn nhất
+
+
+#Bài tập 1: Luyện tập các hàm thông dụng trong SQL
+#a)
+select * from subject where Credit = (select max(Credit) from subject);
+
+#b)
+select S.StudentName, Sub.SubName, M.Mark
+from student S
+join mark M on S.StudentId = M.StudentId
+join subject Sub on M.SubId = Sub.SubId
+where M.Mark = (select max(Mark) from mark); 
+
+#c)
+select S.StudentId, S.StudentName, AVG(M.Mark) as DiemTrungBinh
+from student S
+join mark M on S.StudentId = M.StudentId
+group by S.StudentId, S.StudentName
+order by DiemTrungBinh desc;
